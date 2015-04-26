f608e4ba8b6c9949bf942b5624d7cb2c8ec6bb1c
Kevin Cox <kevintcox@gmail.com>
2015-04-26 15:03:55 -0700

    much improved physics, about to get funky with pole position

---
+++ Assets/Boat.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+//	void OnCollisionEnter(Collision c) {
+//		Debug.Log("boat collided with " + c.gameObject.name);
+//	}
+//	void OnCollisionStay(Collision c) {
+//		Debug.Log("boat stayed colliding with " + c.gameObject.name);
+//	}
+//	void OnTriggerEnter(Collider c) {
+//		Debug.Log("boat trigger enter with " + c.gameObject.name);
+//	}
+
+}
--- Assets/WaterInput.cs
+++ Assets/WaterInput.cs
@@ -5,26 +5,45 @@ using System.Collections;
 public class WaterInput : MonoBehaviour {
 
 	public GameObject boat;
+	public Transform poleTransform;
 	public float paddleEnergyScalar = 50f;
 
-	private Rigidbody boatrb;
+	private Rigidbody boatRigidbody;
 
 	void Start () {
-		boatrb = boat.GetComponentInChildren<Rigidbody>();
+		boatRigidbody = boat.GetComponentInChildren<Rigidbody>();
 	}
 	
 	void Update () {
 		if (Input.GetButtonDown("Fire1")) {
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			if (Physics.Raycast(ray, out hit)) {
-				Debug.Log("hit " + hit.transform.gameObject.name + " at " + hit.point.ToString());
+			RaycastHit waterHit;
+			if (Physics.Raycast(ray, out waterHit)) {
+//				Debug.Log("hit " + waterHit.transform.gameObject.name + " at " + waterHit.point.ToString());
 
-				Vector3 direction = boat.transform.position - hit.point;
-				Debug.DrawRay(hit.point, direction, Color.green, 5f);
+				Vector3 direction = boat.transform.position - waterHit.point;
+				Debug.DrawRay(waterHit.point, direction, Color.green, 5f);
 
-				boatrb.AddForceAtPosition(direction * paddleEnergyScalar, hit.point);
+				// now raycast from water hit point to boat to find position we hit it
+				Ray boatRay = new Ray(waterHit.point, direction);
+				RaycastHit boatHit;
+				if (Physics.Raycast(boatRay, out boatHit)) {
+					Debug.DrawRay(boatHit.point, waterHit.point - boatHit.point, Color.red, 5f);
+					boatRigidbody.AddForceAtPosition(direction * paddleEnergyScalar, boatHit.point);
+				}
+
+//				EditorApplication.isPaused = true;
 			}
 		}
 	}
+
+//	void OnCollisionEnter(Collision c) {
+//		Debug.Log("water collided with " + c.gameObject.name);
+//	}
+//	void OnCollisionStay(Collision c) {
+//		Debug.Log("water stayed colliding with " + c.gameObject.name);
+//	}
+//	void OnTriggerEnter(Collider c) {
+//		Debug.Log("water trigger enter with " + c.gameObject.name);
+//	}
 }
