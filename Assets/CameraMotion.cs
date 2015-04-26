8b88be503e52f64dc52936f0eb1af371a4afc357
Kevin Cox <kevintcox@gmail.com>
2015-04-26 15:58:32 -0700

    steps towards being a game -- endless-ish river instead of box

---
+++ Assets/CameraMotion.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMotion : MonoBehaviour {
+
+	public GameObject boat;
+
+	private float camBoatYDifference;
+
+	void Start () {
+		camBoatYDifference = this.transform.position.y - boat.transform.position.y;
+	}
+	
+	void Update () {
+		Vector3 pos = this.transform.position;
+		pos.y = boat.transform.position.y + camBoatYDifference;
+		this.transform.position = pos;
+	}
+}
