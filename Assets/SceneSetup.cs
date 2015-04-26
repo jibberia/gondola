a16d2947b2f35e7a3a316c4716a3a3bc27003874
Kevin Cox <kevintcox@gmail.com>
2015-04-26 16:53:47 -0700

    slightly randomized detritus scale

--- Assets/SceneSetup.cs
+++ Assets/SceneSetup.cs
@@ -10,11 +10,21 @@ public class SceneSetup : MonoBehaviour {
 		const int n = 400;
 		const float h = 1000f;
 		const float w = 2f;
+		const float sv = .2f; // scale variance
 
 		for (int i=0; i<n; i++) {
 			Vector3 pos = new Vector3(Random.Range(-w, w), Random.Range(0, h));
 			Quaternion rot = new Quaternion(0, 1, 0, Random.Range(0, 90));
+
 			GameObject detritus = (GameObject)Instantiate(detritusPrefab, pos, rot);
+
+			Vector3 scale = detritus.transform.localScale;
+			float rsv = Random.Range(-sv, sv);
+			scale.x += rsv;
+			scale.y += rsv;
+			scale.z += rsv;
+			detritus.transform.localScale = scale;
+
 			detritus.transform.SetParent(detritusParent.transform);
 		}
 	}
