2f8b3873e42dd44af7a0713a7e539c9523d3190f
Kevin Cox <kevintcox@gmail.com>
2015-04-26 13:56:36 -0700

    motion in progress, about to consider rotating everything to the unity way

---
+++ Assets/WaterInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterInput : MonoBehaviour {
+
+	public GameObject boat;
+
+	private Rigidbody boatrb;
+
+	void Start () {
+		boatrb = boat.GetComponentInChildren<Rigidbody>();
+	}
+	
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)) {
+				Debug.Log("hit " + hit.transform.gameObject.name + " at " + hit.point.ToString());
+
+				Vector3 direction = hit.point - boat.transform.position;
+
+				boatrb.AddForceAtPosition(direction.normalized, hit.point);
+			}
+		}
+	}
+}
